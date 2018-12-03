5da12e3db735cddb0c0ac8ade4d3695e8cabcba7
g2ek <lewheonesine@gmail.com>
2018-12-03 21:41:44 +0100

    End of coding important features for the calculator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  value: string = "";
   constructor(public navCtrl: NavController) {
 
   }
 
+    takeValue(value)
+    {
+      this.value = this.value+""+value;
+    }
+
+    backSpace()
+    {
+      this.value = this.value.substr(0,this.value.length-1);
+    }
+
+    deleteAll()
+    {
+      this.value = "";
+    }
+
+    takeDiv()
+    {
+        if(this.value.length != 0 && this.value.substr(this.value.length -1) != '*' && this.value.substr(this.value.length -1) != '-' && this.value.substr(this.value.length -1) != '+' && this.value.substr(this.value.length -1) != '/')
+          this.value = this.value+"/";
+    }
+
+    takeAdiction()
+    {
+        if(this.value.length != 0 && this.value.substr(this.value.length -1) != '*' && this.value.substr(this.value.length -1) != '-' && this.value.substr(this.value.length -1) != '+' && this.value.substr(this.value.length -1) != '/')
+          this.value = this.value+"+";
+    }
+
+    takeSubstraction()
+    {
+        if(this.value.length != 0 && this.value.substr(this.value.length -1) != '*' && this.value.substr(this.value.length -1) != '-' && this.value.substr(this.value.length -1) != '+' && this.value.substr(this.value.length -1) != '/')
+          this.value = this.value+"-";
+    }
+
+    takeMutiplication()
+    {
+        if(this.value.length != 0 && this.value.substr(this.value.length -1) != '*' && this.value.substr(this.value.length -1) != '-' && this.value.substr(this.value.length -1) != '+' && this.value.substr(this.value.length -1) != '/')
+          this.value = this.value+"*";
+    }
+
+    equal()
+    {
+        this.value = eval(this.value);
+    }
 }
